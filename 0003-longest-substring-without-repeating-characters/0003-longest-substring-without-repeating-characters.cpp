ad1bef3f465b23e1879882283906d4135ffba60e
Manojprabhu R <149041648+manoj01-1@users.noreply.github.com>
2024-08-27 22:06:18 +0530

    Time: 3 ms (95.41%), Space: 11.2 MB (67.94%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,37 +1,19 @@
-#include <string>
-#include <unordered_set>
-using namespace std;
-
-int longestsub(string s, int start) {
-    int count = 0;
-    unordered_set<char> hash;
-
-    for (int i = start; i < s.length(); i++) {
-        if (hash.find(s[i]) == hash.end()) {
-            hash.insert(s[i]);
-            count++;
-        } else {
-            hash.clear();
-            return count;
-        }
-    }
-
-    return count;
-}
-
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int maxlen = 0;
+        int maxlen = 0, l = 0; 
+        vector<int> hash(256, -1); 
 
-        for (int i = 0; i < s.length(); i++) {
-            int temp = longestsub(s, i);
-            if (temp > maxlen)
-                maxlen = temp;
-            if (temp == 0)
-                break;
+        for (int r = 0; r < s.length(); r++) {
+            
+            if (hash[s[r]] >= l) {
+                l = hash[s[r]] + 1;
+            }
+
+            hash[s[r]] = r; 
+            maxlen = max(maxlen, r - l + 1); 
         }
 
-        return maxlen;
+        return maxlen; 
     }
 };
